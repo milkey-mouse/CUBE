6b8e5b7ae4b93600d5a0a106f939436bb7cca041
Milkey Mouse <sillystixco@hotmail.com>
2015-11-13 22:36:56 -0800

    Added launcher source
    It's a heavily modified version of the Adlez launcher source code

---
+++ POLYVERSAL INITIALIZER/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace CUBE_Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Initialize = new System.ComponentModel.BackgroundWorker();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LoadingIcon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LoadingIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.BackColor = System.Drawing.Color.Transparent;
+            this.Label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Label1.Font = new System.Drawing.Font("Lucida Console", 24F);
+            this.Label1.ForeColor = System.Drawing.Color.White;
+            this.Label1.Location = new System.Drawing.Point(0, 0);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(191, 736);
+            this.Label1.TabIndex = 8;
+            this.Label1.Text = "CUBE v. 🐟\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n";
+            this.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Initialize
+            // 
+            this.Initialize.WorkerSupportsCancellation = true;
+            this.Initialize.DoWork += new System.ComponentModel.DoWorkEventHandler(this.Initialize_DoWork);
+            this.Initialize.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.Initialize_RunWorkerCompleted);
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(12, 376);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(570, 23);
+            this.Button1.TabIndex = 7;
+            this.Button1.Text = "Start CUBE";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.PictureBox1.Image = global::CUBE_Launcher.Properties.Resources._1;
+            this.PictureBox1.Location = new System.Drawing.Point(183, 0);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(411, 411);
+            this.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox1.TabIndex = 9;
+            this.PictureBox1.TabStop = false;
+            // 
+            // LoadingIcon
+            // 
+            this.LoadingIcon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.LoadingIcon.Image = ((System.Drawing.Image)(resources.GetObject("LoadingIcon.Image")));
+            this.LoadingIcon.Location = new System.Drawing.Point(0, 425);
+            this.LoadingIcon.Name = "LoadingIcon";
+            this.LoadingIcon.Size = new System.Drawing.Size(594, 411);
+            this.LoadingIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.LoadingIcon.TabIndex = 6;
+            this.LoadingIcon.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(594, 411);
+            this.Controls.Add(this.LoadingIcon);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.PictureBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(610, 450);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(610, 450);
+            this.Name = "Form1";
+            this.Text = "CUBE Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LoadingIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label Label1;
+        internal System.ComponentModel.BackgroundWorker Initialize;
+        internal System.Windows.Forms.Button Button1;
+        internal System.Windows.Forms.PictureBox PictureBox1;
+        internal System.Windows.Forms.PictureBox LoadingIcon;
+    }
+}
+
---
+++ POLYVERSAL INITIALIZER/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace CUBE_Launcher
+{
+    public partial class Form1 : Form
+    {
+        string appdata = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        string documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        System.Net.WebClient wc = new System.Net.WebClient();
+
+        private void Initialize_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            if (!System.IO.Directory.Exists(appdata + "\\CUBE"))
+            {
+                System.IO.Directory.CreateDirectory(appdata + "\\CUBE");
+            }
+            if (!System.IO.Directory.Exists(documents + "\\Zero"))
+            {
+                System.IO.Directory.CreateDirectory(documents + "\\Zero");
+            }
+            if(!System.IO.File.Exists(documents + "\\Zero\\ConfigurationV3.data"))
+            {
+                System.IO.File.WriteAllText(documents + "\\Zero\\ConfigurationV3.data", wc.DownloadString("http://team-ivan.com/blog/ConfigurationV3.txt").Replace("<reswidth>", SystemInformation.PrimaryMonitorSize.Width.ToString()).Replace("<resheight>", SystemInformation.PrimaryMonitorSize.Height.ToString()));
+            }
+            try
+            {
+                wc.DownloadFile("http://team-ivan.com/blog/cube_welcome.txt", appdata + "\\CUBE\\welcome.txt");
+            } catch { }
+            if (System.IO.Directory.Exists(appdata + "\\CUBE") == false)
+            {
+                System.IO.Directory.CreateDirectory(appdata + "\\CUBE");
+            }
+            try
+            {
+                if (!System.IO.File.Exists(appdata + "\\CUBE\\rev.txt") || !(wc.DownloadString("http://team-ivan.com/blog/cube_rev.txt") == System.IO.File.ReadAllText(appdata + "\\CUBE\\rev.txt")))
+                {
+                    wc.DownloadFile("http://team-ivan.com/blog/CUBE.exe", appdata + "\\CUBE\\CUBE.exe");
+                }
+            }
+            catch { }
+            try
+            {
+                wc.DownloadFile("http://team-ivan.com/blog/cube_rev.txt", appdata + "\\CUBE\\rev.txt");
+            } catch { }
+            if (System.IO.Directory.Exists(appdata + "\\CUBE") == false)
+            {
+                System.IO.Directory.CreateDirectory(appdata + "\\CUBE");
+            }
+            if(!System.IO.File.Exists(appdata + "\\CUBE\\CUBE.exe"))
+            {
+                Label1.Text = "Connect to\r\nthe Internet!";
+            }
+        }
+
+        private string RandImage()
+        {
+            while (true)
+            {
+                int time = Math.Abs((int)(DateTime.Now.Ticks / 1000) % 9);
+                if (!(time == 9) && !(time == 0))
+                {
+                    return time.ToString();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadingIcon.BringToFront();
+            LoadingIcon.Visible = true;
+            LoadingIcon.Location = new System.Drawing.Point(0, 0);
+            Button1.Enabled = false;
+            PictureBox1.Image = (System.Drawing.Image)CUBE_Launcher.Properties.Resources.ResourceManager.GetObject("_" + RandImage());
+            Initialize.RunWorkerAsync();
+        }
+
+        private void Initialize_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            try
+            {
+                Label1.Text = System.IO.File.ReadAllText(appdata + "\\CUBE\\welcome.txt");
+                for (int i = 0; i <= 20; i++)
+                {
+                    Label1.Text += "\r\n";
+                }
+            }
+            catch { }
+            LoadingIcon.Visible = false;
+            LoadingIcon.Location = new System.Drawing.Point(0, 425);
+            Button1.Enabled = true;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists(appdata + "\\CUBE\\CUBE.exe") == false)
+            {
+                MessageBox.Show("You need to have connected to the Internet at least once to play CUBE!");
+            }
+            else
+            {
+                try
+                {
+                    Process loa = Process.Start(appdata + "\\CUBE\\CUBE.exe", "-nolauncher");
+                    loa.PriorityClass = ProcessPriorityClass.AboveNormal; //"optimizes" for crappy hardware
+                    this.Close();
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
